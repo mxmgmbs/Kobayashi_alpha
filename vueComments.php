905cca92636abf23e5eb2ba8a32c234fa04b914e
PeyoKobayashi <32135487+PeyoKobayashi@users.noreply.github.com>
2018-03-02 23:18:37 +0100

    Fixed comments with usernames

---
+++ vueComments.php
@@ -0,0 +1,51 @@
+<html>
+	<body>
+	<center>
+		<br><br><br><br>
+		<div class="container">
+		<table class ="table">
+
+<?php 
+		//parcours des resultats avec foreach
+		foreach ($resultatsComments as $unResultat) 
+		{
+			//var_dump($unResultat['vidChemin']);
+			echo "
+					<p>".$unResultat['comContent']."</p>
+					<p>".$unResultat['comDate']."</p>
+					<p>".$unResultat['comHour']. "</p>
+					<p>".$unResultat['comAutorName']."</p>"
+				;
+		}		
+
+			if(isset($_SESSION['id'])){
+				include ("vue/vueInsertComment.php");
+				if(isset($_POST['valider']))
+				{
+					//print("nom : ". $_POST['nom']."<br>");
+					//print("email : ". $_POST['email']."<br>");
+					//print("date de naissance : ". $_POST['dateNaiss']."<br>");
+					//print("mot de passe : ". $_POST['mdp']."<br>");
+					//print("verif mdp : ". $_POST['mdp2']."<br>");
+
+					//jusque la ok
+					
+					$unControleur->insertComment($_POST['content'] ,$_SESSION['id'], $_GET['id']);
+				}
+			} else {
+				print("veuillez vous connecter pour poster un commentaire");
+			}
+
+
+
+
+
+
+?>
+
+
+			</table>
+			</div>
+		</center>
+	</body>
+</html>
